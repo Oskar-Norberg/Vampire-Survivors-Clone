40ca0521665b75d8cea59838af6db8649aa298c0
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-10 14:18:06 +0200

    Remove tag based attack, handled by collision

--- Assets/Scripts/Components/Attack.cs
+++ Assets/Scripts/Components/Attack.cs
@@ -7,17 +7,8 @@ public class Attack : MonoBehaviour
     [Header("Attack Properties")]
     [SerializeField] private int damage = 1;
     
-    [Header("Tags to Attack")]
-    [SerializeField] private bool attackPlayer;
-    [SerializeField] private bool attackEnemy;
-    
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Attack if collision tag matches attack specified tags
-        bool attack = other.CompareTag("Player") && attackPlayer || other.CompareTag("Enemy") && attackEnemy;
-
-        if (!attack) return;
-
         // Try to get health component in parent and apply attack damage
         if (other.transform.parent.TryGetComponent<Health>(out Health health))
         {
