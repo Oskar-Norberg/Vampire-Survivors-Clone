c6dd483a0e8050c1294fa990279a5dc3e6e8417c
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-10 13:01:11 +0200

    Move code from EnemyBase to attack component

--- Assets/Scripts/Components/Attack.cs
+++ Assets/Scripts/Components/Attack.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    [Header("Attack Properties")]
     [SerializeField] private int damage = 1;
+    
+    [Header("Tags to Attack")]
+    [SerializeField] private bool attackPlayer;
+    [SerializeField] private bool attackEnemy;
     
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (!other.gameObject.CompareTag("Player")) return;
-        
-        other.gameObject.GetComponentInParent<Health>().TakeDamage(damage);
+        // Attack if collision tag matches attack specified tags
+        bool attack = other.CompareTag("Player") && attackPlayer || other.CompareTag("Enemy") && attackEnemy;
+
+        if (!attack) return;
+
+        // Try to get health component in parent and apply attack damage
+        if (other.transform.parent.TryGetComponent<Health>(out Health health))
+        {
+            health.TakeDamage(damage);
+        }
     }
 }
