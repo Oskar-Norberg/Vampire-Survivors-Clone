4e8c8e9eddfd8336153d815f0779ba858b1deb75
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-10 14:00:12 +0200

    Implement basic lasso

---
+++ Assets/Scripts/Weapons/Lasso.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Lasso : WeaponBase
+{
+    [SerializeField] private float attackDurationMilliseconds = 500.0f;
+    
+    [SerializeField] private BoxCollider2D hitbox;
+    [SerializeField] private SpriteRenderer sprite;
+
+    private void Start()
+    {
+        SetAttackStatus(false);
+    }
+
+    protected override void Activate()
+    {
+        StartCoroutine(Attack());
+    }
+
+    private IEnumerator Attack()
+    {
+        SetAttackStatus(true);
+        yield return new WaitForSeconds(attackDurationMilliseconds / 1000);
+        SetAttackStatus(false);
+        StartCoroutine(StartCooldown());
+    }
+
+    private void SetAttackStatus(bool status)
+    {
+        if (status)
+        {
+            sprite.enabled = true;
+            hitbox.enabled = true;
+        }
+        else
+        {
+            hitbox.enabled = false;
+            sprite.enabled = false;
+        }
+    }
+}
