f8dd97b8ab0162d8f1a987dc029211a785775197
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-10 13:11:56 +0200

    Add lerp to cameraHolder

--- Assets/Scripts/Camera/CameraHolder.cs
+++ Assets/Scripts/Camera/CameraHolder.cs
@@ -4,4 +4,24 @@ using UnityEngine;
 
 public class CameraHolder : MonoBehaviour
 {
+    [SerializeField] private Transform target;
+    
+    [Header("Lerp Settings")]
+    [SerializeField] private bool useLerp;
+    [SerializeField] private float lerpWeight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!target) return;
+        
+        if (useLerp)
+        {
+            transform.position = Vector2.Lerp(transform.position, target.position, lerpWeight * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = target.position;
+        }
+    }
 }
