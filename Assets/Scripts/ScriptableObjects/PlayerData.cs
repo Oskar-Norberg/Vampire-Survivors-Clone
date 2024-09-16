f07decaba2ea248ce34a82345fbad46e5e134349
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-16 11:59:40 +0200

    Create PlayerData scriptable object

---
+++ Assets/Scripts/ScriptableObjects/PlayerData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player Data")]
+public class PlayerData : ScriptableObject
+{
+    public int health;
+    public float moveSpeed;
+}
