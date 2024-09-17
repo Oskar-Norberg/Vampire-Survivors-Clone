901ea8a2301ddbd6292c28b8487448b1e06a2c56
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-17 15:11:52 +0200

    Spawn enemies through enemy manager

---
+++ Assets/Scripts/Enemies/EnemySpawner/EnemySpawner.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private EnemyManager enemyManager;
+    
+    [SerializeField] private List<GameObject> enemiesToSpawn = new List<GameObject>();
+    
+    [Header("Spawn Position")]
+    [SerializeField] private float minDistanceFromPlayer;
+    [SerializeField] private float maxDistanceFromPlayer;
+    
+    [Header("Spawn Properties")]
+    [SerializeField] private float timeBetweenSpawnsMilliseconds;
+    [SerializeField] private int enemiesPerSpawn;
+
+    private Transform playerTransform;
+
+    private void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        StartCoroutine(WaveCoroutine());
+    }
+    
+    // Spawns enemies in a circle around the player
+    private IEnumerator WaveCoroutine()
+    {
+        while (true)
+        {
+            for (int i = 0; i < enemiesPerSpawn; i++)
+            {
+                SpawnRandomEnemy();
+            }
+            yield return new WaitForSeconds(timeBetweenSpawnsMilliseconds / 1000);
+        }
+    }
+
+    private void SpawnRandomEnemy()
+    {
+        if (playerTransform == null) return;
+
+        Vector2 randomDirection = Random.insideUnitCircle.normalized;
+
+        Vector2 distanceFromPlayer = randomDirection * Random.Range(minDistanceFromPlayer, maxDistanceFromPlayer);
+        
+        GameObject enemy = enemiesToSpawn[(int)Random.Range(0, enemiesToSpawn.Count)];
+        
+        Vector2 enemyPosition = distanceFromPlayer + (Vector2) playerTransform.position;
+        
+        enemyManager.SpawnEnemy(enemy, enemyPosition);
+    }
+}
