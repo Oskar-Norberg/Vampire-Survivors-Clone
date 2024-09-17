ac9dc298918dfe7bc393f5705fbdd747149de2c2
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-17 15:17:05 +0200

    Refactor CameraHolder to FollowCamera

--- Assets/Scripts/Camera/FollowCamera.cs
+++ Assets/Scripts/Camera/FollowCamera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraHolder : MonoBehaviour
+public class FollowCamera : MonoBehaviour
 {
     [SerializeField] private Transform target;
     
