666fb663ff6953e2f41bffc93729b19eff98a4b9
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-17 09:14:08 +0200

    Fix logical error in enemy spawning

--- Assets/Scripts/EnemySpawner/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner/EnemySpawner.cs
@@ -33,8 +33,8 @@ public class EnemySpawner : MonoBehaviour
             for (int i = 0; i < enemiesPerSpawn; i++)
             {
                 SpawnRandomEnemy();
-                yield return new WaitForSeconds(timeBetweenSpawnsMilliseconds / 1000);
             }
+            yield return new WaitForSeconds(timeBetweenSpawnsMilliseconds / 1000);
         }
     }
 
