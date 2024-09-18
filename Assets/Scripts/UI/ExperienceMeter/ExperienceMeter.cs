d12e8534fa3d04cc88d96bc5285f276150e14c41
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-18 15:13:32 +0200

    Update UI on experience change

--- Assets/Scripts/UI/ExperienceMeter/ExperienceMeter.cs
+++ Assets/Scripts/UI/ExperienceMeter/ExperienceMeter.cs
@@ -6,4 +6,20 @@ using UnityEngine.UI;
 public class ExperienceMeter : MonoBehaviour
 {
     [SerializeField] private Slider slider;
+
+    private void OnEnable()
+    {
+        ExperienceManager.onExperienceChanged += UpdateSlider;
+    }
+    
+    private void OnDisable()
+    {
+        ExperienceManager.onExperienceChanged -= UpdateSlider;
+    }
+
+    private void UpdateSlider()
+    {
+        slider.maxValue = ExperienceManager.instance.GetExperiencePerLevel();
+        slider.value = ExperienceManager.instance.GetExperience();
+    }
 }
