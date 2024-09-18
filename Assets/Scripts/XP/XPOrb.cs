e904a02c20f8c61fa6d429e02b5d8a3f19fc457d
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-18 12:54:21 +0200

    Basic Pick up xp script

---
+++ Assets/Scripts/XP/XPOrb.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XPOrb : MonoBehaviour
+{
+    public void Pickup()
+    {
+        Destroy(gameObject);
+        throw new NotImplementedException("XP Orb has not been implemented");
+    }
+}
