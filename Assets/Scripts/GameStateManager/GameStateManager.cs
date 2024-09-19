2112924411ad81de4fc896c46442678820727c10
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-19 11:38:26 +0200

    Start GameStateManager in Playing

--- Assets/Scripts/GameStateManager/GameStateManager.cs
+++ Assets/Scripts/GameStateManager/GameStateManager.cs
@@ -7,6 +7,18 @@ public class GameStateManager : MonoBehaviour
 {
     [SerializeField] private List<BaseGameState> states = new List<BaseGameState>();
     private BaseGameState currentState;
+    private void Start()
+    {
+        // Start in Playing Game State
+        foreach (BaseGameState state in states)
+        {
+            if (state.GetType() == typeof(PlayingGameState))
+            {
+                currentState = state;
+            }
+        }
+    }
+
     public void SwitchState<TNextState>()
     {
         foreach (BaseGameState state in states)
