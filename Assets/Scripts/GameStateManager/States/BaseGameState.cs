3cf4d994cb1861c61ea2f522f4e91544e20e31f2
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-19 11:38:47 +0200

    Create BaseGameState

---
+++ Assets/Scripts/GameStateManager/States/BaseGameState.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseGameState : MonoBehaviour
+{
+    public abstract void EnterState();
+    
+    public abstract void ExitState();
+
+    public abstract void UpdateState();
+}
