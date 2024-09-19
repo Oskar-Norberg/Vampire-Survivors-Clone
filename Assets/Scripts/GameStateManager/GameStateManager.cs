f712b56156b3ce54270eb3cb8d7ce68759f8bbd6
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-19 11:38:13 +0200

    Create SwitchState function

--- Assets/Scripts/GameStateManager/GameStateManager.cs
+++ Assets/Scripts/GameStateManager/GameStateManager.cs
@@ -5,4 +5,20 @@ using UnityEngine;
 
 public class GameStateManager : MonoBehaviour
 {
+    [SerializeField] private List<BaseGameState> states = new List<BaseGameState>();
+    private BaseGameState currentState;
+    public void SwitchState<TNextState>()
+    {
+        foreach (BaseGameState state in states)
+        {
+            if (state.GetType() == typeof(TNextState))
+            {
+                currentState?.ExitState();
+                currentState = state;
+                state.EnterState();
+                return;
+            }
+        }
+        Debug.Log("State not found");
+    }
 }
