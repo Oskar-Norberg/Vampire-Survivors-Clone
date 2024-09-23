d7de07c30cef747d5b49a5b586f57b60720a4dcc
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-23 15:53:51 +0200

    Playing Game State transition to Upgrade state

--- Assets/Scripts/GameStateManager/States/PlayingGameState.cs
+++ Assets/Scripts/GameStateManager/States/PlayingGameState.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 
 public class PlayingGameState : BaseGameState
 {
-
+    // Foul hack, but the only way to pass gameStateManager to OnLevelUp function.
+    private GameStateManager gameStateManager;
     
     public override void EnterState(GameStateManager gameStateManager)
     {
-        
+        this.gameStateManager = gameStateManager;
+        ExperienceManager.onLevelUp += OnLevelUp;
     }
 
     public override void ExitState(GameStateManager gameStateManager)
     {
-        
+        ExperienceManager.onLevelUp -= OnLevelUp;
     }
 
     public override void FixedUpdateState(GameStateManager gameStateManager)
@@ -30,5 +32,14 @@ public class PlayingGameState : BaseGameState
         {
             gameStateManager.SwitchState<PausedGameState>();
         }
+    }
+
+    private void OnLevelUp()
+    {
+        if (!gameStateManager)
+        {
+            Debug.Log("Game State Manager missing in PlayingGameState, skipping level up");
+        }
+        gameStateManager.SwitchState<UpgradeState>();
     }
 }
