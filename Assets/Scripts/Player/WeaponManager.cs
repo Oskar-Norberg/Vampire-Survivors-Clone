41d68d9564c3c83d20c219094dd9809c53b16acd
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-23 13:13:23 +0200

    Create Player Weapon Manager

---
+++ Assets/Scripts/Player/WeaponManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    private List<WeaponBase> weapons = new List<WeaponBase>();
+    
+    public void AddWeapon(GameObject weapon)
+    {
+        GameObject instance = Instantiate(weapon, transform);
+        weapons.Add(instance.GetComponent<WeaponBase>());
+    }
+    
+    public void FixedUpdateWeapons()
+    {
+        foreach (WeaponBase weapon in weapons)
+        {
+            weapon.FixedUpdateMovement();
+        }
+    }
+}
