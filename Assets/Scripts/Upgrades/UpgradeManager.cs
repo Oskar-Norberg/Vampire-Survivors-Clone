584313a07e3404e644e87b391c148c9ff7b1d27f
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-23 11:21:12 +0200

    Apply random upgrade on level up

--- Assets/Scripts/Upgrades/UpgradeManager.cs
+++ Assets/Scripts/Upgrades/UpgradeManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class UpgradeManager : MonoBehaviour
 {
@@ -14,5 +15,22 @@ public class UpgradeManager : MonoBehaviour
     private void Start()
     {
         upgrades = Resources.LoadAll<Upgrade>(UpgradePath);
+    }
+
+    private void OnEnable()
+    {
+        ExperienceManager.onLevelUp += OnLevelUp;
+    }
+    
+    private void OnDisable()
+    {
+        ExperienceManager.onLevelUp -= OnLevelUp;
+    }
+
+    private void OnLevelUp()
+    {
+        int index = Random.Range(0, upgrades.Length);
+        
+        upgrades[index].Apply(player.gameObject);
     }
 }
