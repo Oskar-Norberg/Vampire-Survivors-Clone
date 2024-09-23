14d07824604cf2e1ae1a8c56886bc8574abe2e60
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-23 14:31:16 +0200

    Add upgrade to WeaponBase

--- Assets/Scripts/Weapons/WeaponBase.cs
+++ Assets/Scripts/Weapons/WeaponBase.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 
 public abstract class WeaponBase : MonoBehaviour
 {
+    protected int upgrade = 0;
+    
     [SerializeField] protected WeaponData weaponData;
     protected enum WeaponStates {Ready, OnCooldown}
 
     protected WeaponStates State = WeaponStates.Ready;
 
     public abstract void FixedUpdateMovement();
+
+    public virtual void Upgrade()
+    {
+        upgrade++;
+    }
 }
