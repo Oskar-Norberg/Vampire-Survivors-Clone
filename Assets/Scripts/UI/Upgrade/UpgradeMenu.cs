8df54a34e6aa3100dbc099d067e7ad8307ec8075
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-23 16:26:14 +0200

    Upgrade menu

---
+++ Assets/Scripts/UI/Upgrade/UpgradeMenu.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeMenu : MonoBehaviour
+{
+    [SerializeField] private UpgradeManager upgradeManager;
+    
+    [SerializeField] private List<UpgradeCard> upgradeCards = new List<UpgradeCard>();
+    
+    public delegate void UpgradeSelectedDelegate(Upgrade upgrade);
+    public static event UpgradeSelectedDelegate upgradeSelectedDelegate;
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < upgradeCards.Count; i++)
+        {
+            upgradeCards[i].SetUpgrade(upgradeManager.GetRandomUpgrade());
+        }
+
+        UpgradeCard.onClick += SelectUpgradeCard;
+    }
+
+    private void OnDisable()
+    {
+        UpgradeCard.onClick -= SelectUpgradeCard;
+    }
+
+    private void SelectUpgradeCard(Upgrade upgrade)
+    {
+        upgradeManager.ApplyUpgrade(upgrade);
+        upgradeSelectedDelegate?.Invoke(upgrade);
+    }
+}
