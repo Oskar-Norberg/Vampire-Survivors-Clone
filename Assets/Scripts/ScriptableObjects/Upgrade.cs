f02bec17fe71fc25849b3b26dc67855ce2f147da
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-23 14:43:07 +0200

    Add Name and description to Upgrades

--- Assets/Scripts/ScriptableObjects/Upgrade.cs
+++ Assets/Scripts/ScriptableObjects/Upgrade.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Upgrade : ScriptableObject
 {
+    public String name;
+    public String description;
+    
     public abstract void Apply(GameObject target);
 }
