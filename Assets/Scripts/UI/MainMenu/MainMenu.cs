06f79b74728bbb97633b14be03ec1992e6d0d964
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-24 11:08:21 +0200

    Hide back button when in main menu state

--- Assets/Scripts/UI/MainMenu/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu/MainMenu.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
     enum MainMenuState { MainMenu, LevelSelect, OptionsMenu }
-    
+
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject levelSelect;
     [SerializeField] private GameObject optionsMenu;
+    [SerializeField] private GameObject backButton;
 
     private void SetState(MainMenuState state)
     {
         mainMenu.SetActive(state == MainMenuState.MainMenu);
         levelSelect.SetActive(state == MainMenuState.LevelSelect);
         optionsMenu.SetActive(state == MainMenuState.OptionsMenu);
+        
+        backButton.SetActive(state != MainMenuState.MainMenu);
     }
 
     public void GoToMainMenu()
