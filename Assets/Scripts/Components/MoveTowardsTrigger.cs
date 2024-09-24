f8b64b095583528489a842dfe56f42e8b6483803
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-24 15:11:35 +0200

    Pause MoveTowardsTrigger

--- Assets/Scripts/Components/MoveTowardsTrigger.cs
+++ Assets/Scripts/Components/MoveTowardsTrigger.cs
@@ -3,13 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveTowardsTrigger : MonoBehaviour
+public class MoveTowardsTrigger : PausableMonoBehaviour
 {
     [SerializeField] private new Rigidbody2D rigidbody;
     [SerializeField] private float movementSpeed;
     [SerializeField] private IAIStrategy strategy;
     
     private Transform target;
+
+    private bool isPaused = false;
+    private Vector2 prePauseVelocity;
     
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -18,11 +21,23 @@ public class MoveTowardsTrigger : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (!rigidbody) return;
-        if (!target) return;
+        if (!rigidbody || !target || isPaused) return;
 
         Vector2 wishDir = strategy.PathFind(transform.position, target.position);
         
         rigidbody.AddForce(wishDir * movementSpeed);
+    }
+
+    protected override void Pause()
+    {
+        isPaused = true;
+        prePauseVelocity = rigidbody.velocity;
+        rigidbody.velocity = Vector2.zero;
+    }
+
+    protected override void UnPause()
+    {
+        isPaused = false;
+        rigidbody.velocity = prePauseVelocity;
     }
 }
