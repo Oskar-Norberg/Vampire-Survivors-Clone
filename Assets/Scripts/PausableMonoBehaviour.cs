db172b07f69f943d774b8888e759ccaabe5695d6
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-24 14:41:03 +0200

    Subscribe to pause events in PausableMonoBehaviour

--- Assets/Scripts/PausableMonoBehaviour.cs
+++ Assets/Scripts/PausableMonoBehaviour.cs
@@ -1,9 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class PausableMonoBehaviour : MonoBehaviour
 {
-    public abstract void Pause();
-    public abstract void UnPause();
+    private void OnEnable()
+    {
+        PausedGameState.onPause += Pause;
+        PausedGameState.onResume += UnPause;
+    }
+    
+    private void OnDisable()
+    {
+        PausedGameState.onPause -= Pause;
+        PausedGameState.onResume -= UnPause;
+    }
+
+    protected abstract void Pause();
+    protected abstract void UnPause();
 }
