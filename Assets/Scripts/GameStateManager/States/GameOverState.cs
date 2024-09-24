c6a7ed40fa71d7b1a8c7778727be6c79c7f544b5
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-24 16:34:34 +0200

    Show GameOverUI on GameOverState

--- Assets/Scripts/GameStateManager/States/GameOverState.cs
+++ Assets/Scripts/GameStateManager/States/GameOverState.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class GameOverState : BaseGameState
 {
-    private GameStateManager gameStateManager;
-    
     public override void EnterState(GameStateManager gameStateManager)
     {
-        this.gameStateManager = gameStateManager;
+        gameStateManager.gameOverMenu.SetActive(true);
         gameStateManager.Pause();
     }
 
     public override void ExitState(GameStateManager gameStateManager)
     {
+        gameStateManager.gameOverMenu.SetActive(false);
         gameStateManager.Resume();
     }
 
