ae5a61653cb4b9c429a2226918dc0a4da138130a
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-25 10:59:15 +0200

    Goto MainMenu from GameOverState

--- Assets/Scripts/GameStateManager/States/GameOverState.cs
+++ Assets/Scripts/GameStateManager/States/GameOverState.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class GameOverState : BaseGameState
 {
+    private GameStateManager gameStateManager;
+    
     public override void EnterState(GameStateManager gameStateManager)
     {
+        this.gameStateManager = gameStateManager;
         gameStateManager.gameOverMenu.SetActive(true);
         gameStateManager.Pause();
+
+        GameOverMenu.mainMenu += GoToMainMenu;
     }
 
     public override void ExitState(GameStateManager gameStateManager)
     {
         gameStateManager.gameOverMenu.SetActive(false);
         gameStateManager.Resume();
+        GameOverMenu.mainMenu -= GoToMainMenu;
     }
 
     public override void FixedUpdateState(GameStateManager gameStateManager)
@@ -22,5 +28,10 @@ public class GameOverState : BaseGameState
 
     public override void UpdateState(GameStateManager gameStateManager)
     {
+    }
+
+    private void GoToMainMenu()
+    {
+        gameStateManager.GoToMainMenu();
     }
 }
