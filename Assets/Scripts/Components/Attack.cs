233af9dfec77ebfbeaacfb765e9b5637243f6b0f
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-26 15:40:22 +0200

    Change Attack from OnTriggerEnter to Stay

--- Assets/Scripts/Components/Attack.cs
+++ Assets/Scripts/Components/Attack.cs
@@ -7,7 +7,7 @@ public class Attack : PausableMonoBehaviour
     [Header("Attack Properties")]
     private int damage = 0;
     
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         // Try to get health component in parent and apply attack damage
         if (other.transform.parent.TryGetComponent<Health>(out Health health))
