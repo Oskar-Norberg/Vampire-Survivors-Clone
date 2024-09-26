1d267047bd7bc338a2d4280be2e06a6b7757562f
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-26 11:57:34 +0200

    Create Method for upgrading a specific weapon

--- Assets/Scripts/Player/WeaponManager.cs
+++ Assets/Scripts/Player/WeaponManager.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class WeaponManager : MonoBehaviour
 {
@@ -10,6 +13,32 @@ public class WeaponManager : MonoBehaviour
     {
         GameObject instance = Instantiate(weapon, transform);
         weapons.Add(instance.GetComponent<WeaponBase>());
+    }
+
+    public void UpgradeWeapon(GameObject weapon)
+    {
+        WeaponBase foundWeapon = null;
+        
+        Type weaponType = weapon.GetComponent<WeaponBase>().GetType();
+        
+        foreach (WeaponBase playerWeapon in weapons)
+        {
+            Type playerWeaponType = playerWeapon.GetType();
+            if (weaponType == playerWeaponType)
+            {
+                foundWeapon = playerWeapon;
+                break;
+            }
+        }
+
+        if (foundWeapon)
+        {
+            foundWeapon.Upgrade();
+        }
+        else
+        {
+            AddWeapon(weapon);
+        }
     }
 
     public void UpgradeRandomWeapon()
