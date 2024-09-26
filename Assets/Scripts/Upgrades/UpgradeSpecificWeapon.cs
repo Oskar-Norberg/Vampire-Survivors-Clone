8faddd7f40e55acee3fcbd64d7c4a2c0220b12af
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-26 11:57:46 +0200

    Upgrade Specific weapon scriptable object

---
+++ Assets/Scripts/Upgrades/UpgradeSpecificWeapon.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Upgrades/WeaponUpgrade")]
+public class UpgradeSpecificWeapon : Upgrade
+{
+    public GameObject weapon;
+        
+    public override void Apply(GameObject target)
+    {
+        if (target.TryGetComponent<WeaponManager>(out WeaponManager weaponManager))
+        {
+            weaponManager.UpgradeWeapon(weapon);
+        }
+    }
+}
