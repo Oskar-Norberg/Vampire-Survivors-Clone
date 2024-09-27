23911b802cc202f2e97e5b08f66ca83ef72cdd59
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-27 12:15:52 +0200

    New way of grabbing non duplicate upgrades

--- Assets/Scripts/Upgrades/UpgradeManager.cs
+++ Assets/Scripts/Upgrades/UpgradeManager.cs
@@ -21,6 +21,36 @@ public class UpgradeManager : MonoBehaviour
     {
         upgrade.Apply(player.gameObject);
     }
+
+    public Upgrade[] GetNonDuplicateUpgrades(int upgradeCount)
+    {
+        Upgrade[] nonDuplicateUpgrades = new Upgrade[upgradeCount];
+        
+        // Array of all possible indexes
+        int[] upgradeIndexes = new int[upgrades.Length];
+        
+        for (int i = 0; i < upgrades.Length; i++)
+        {
+            upgradeIndexes[i] = i;
+        }
+        
+        // Randomize indexes
+        for (int i = 0; i < upgrades.Length; i++)
+        {
+            int index1 = Random.Range(0, upgradeIndexes.Length);
+            int index2 = Random.Range(0, upgradeIndexes.Length);
+            
+            (upgradeIndexes[index1], upgradeIndexes[index2]) = (upgradeIndexes[index2], upgradeIndexes[index1]);
+        }
+
+        // Grab three first indexes, which are randomized
+        for (int i = 0; i < upgradeCount; i++)
+        {
+            nonDuplicateUpgrades[i] = upgrades[upgradeIndexes[i]];
+        }
+
+        return nonDuplicateUpgrades;
+    }
     
     public Upgrade GetRandomUpgrade()
     {
