45511b71be3279361faa425709237710cf210c3f
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-30 14:03:14 +0200

    Throw exception if Upgrade doesnt find weapon

--- Assets/Scripts/Upgrades/UpgradeSpecificWeapon.cs
+++ Assets/Scripts/Upgrades/UpgradeSpecificWeapon.cs
@@ -1,16 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public abstract class UpgradeSpecificWeapon : Upgrade
 {
     public GameObject weaponPrefab;
-        
+    
     public override void Apply(GameObject target)
     {
         if (target.TryGetComponent<WeaponManager>(out WeaponManager weaponManager))
         {
-            Upgrade(weaponManager.FindWeapon(weaponPrefab));
+            WeaponBase weapon = weaponManager.FindWeapon(weaponPrefab);
+            if (!weapon)
+            {
+                throw new Exception("Could not find weapon.");
+            }
+            Upgrade(weapon);
         }
     }
 
