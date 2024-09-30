0ddf906d24e49e2f31d5a9e697093dc4acaba457
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-09-30 13:23:14 +0200

    Move Upgrades out from WeaponBase

--- Assets/Scripts/Weapons/WeaponBase.cs
+++ Assets/Scripts/Weapons/WeaponBase.cs
@@ -5,40 +5,8 @@ using UnityEngine;
 
 public abstract class WeaponBase : PausableMonoBehaviour
 {
-    private const int MAX_UPGRADE = 3;
-    
-    protected int upgrade = 0;
-    
     [SerializeField] protected WeaponData weaponData;
     protected enum WeaponStates {Ready, OnCooldown}
 
     protected WeaponStates State = WeaponStates.Ready;
-
-    public void Upgrade()
-    {
-        switch (upgrade)
-        {
-            case 0:
-                UpgradeOne();
-                break;
-            case 1:
-                UpgradeTwo();
-                break;
-            case 2:
-                UpgradeThree();
-                break;
-            default:
-                Debug.Log("Invalid upgrade");
-                break;
-        }
-        
-        if (upgrade <= MAX_UPGRADE - 1)
-        {
-            upgrade++;
-        }
-    }
-    
-    protected abstract void UpgradeOne();
-    protected abstract void UpgradeTwo();
-    protected abstract void UpgradeThree();
 }
