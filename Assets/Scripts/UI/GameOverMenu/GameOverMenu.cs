e04cbe6c0d3609dc1935ae0adcffae6995784ffe
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-10-01 11:18:58 +0200

    Update text to match roundtimer on game over

--- Assets/Scripts/UI/GameOverMenu/GameOverMenu.cs
+++ Assets/Scripts/UI/GameOverMenu/GameOverMenu.cs
@@ -1,11 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameOverMenu : MonoBehaviour
 {
+    [SerializeField] private RoundTimer roundTimer;
+    
+    [SerializeField] private TextMeshProUGUI timerText;
+    
     public delegate void MainMenuDelegate();
     public static event MainMenuDelegate mainMenu;
+
+    private void OnEnable()
+    {
+        timerText.text = roundTimer.GetFormattedString();
+    }
 
     public void OnClickMainMenu()
     {
