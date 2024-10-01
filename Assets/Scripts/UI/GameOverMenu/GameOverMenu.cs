0b725c6f8487bf397e54470a07d723ce0b7c7885
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-10-01 11:55:14 +0200

    Display upgrades in GameOverMenu

--- Assets/Scripts/UI/GameOverMenu/GameOverMenu.cs
+++ Assets/Scripts/UI/GameOverMenu/GameOverMenu.cs
@@ -7,8 +7,10 @@ using UnityEngine;
 public class GameOverMenu : MonoBehaviour
 {
     [SerializeField] private RoundTimer roundTimer;
+    [SerializeField] private UpgradeManager upgradeManager;
     
     [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private TextMeshProUGUI upgradeListText;
     
     public delegate void MainMenuDelegate();
     public static event MainMenuDelegate mainMenu;
@@ -16,10 +18,21 @@ public class GameOverMenu : MonoBehaviour
     private void OnEnable()
     {
         timerText.text = roundTimer.GetFormattedString();
+        SetUpgradeList();
     }
 
     public void OnClickMainMenu()
     {
         mainMenu?.Invoke();
+    }
+
+    private void SetUpgradeList()
+    {
+        string text = "";
+        foreach (UpgradeManager.UpgradeStatus upgrade in upgradeManager.GetAllAppliedUpgrades())
+        {
+            text += upgrade.upgrade.name + " x" + upgrade.amount + "\n\n";
+        }
+        upgradeListText.text = text;
     }
 }
