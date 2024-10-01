c8c719231245dc78c6db766062f84f5060fbb4a6
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-10-01 14:41:58 +0200

    Create fields for upgrade name and description

--- Assets/Scripts/Tools/UpgradeCreationTool.cs
+++ Assets/Scripts/Tools/UpgradeCreationTool.cs
@@ -8,12 +8,26 @@ public class UpgradeCreationTool : EditorWindow
 {
     // Start is called before the first frame update
     void Start()
+    private string upgradeName;
+    private string upgradeDescription;
+
     [MenuItem("Tools/Upgrade Creation")]
     public static void ShowWindow()
     {
         EditorWindow.GetWindow<UpgradeCreationTool>("Upgrade Creation Tool");
     }
+    
+    private void OnGUI()
     {
+        GUILayout.Label("Upgrade Creation Tool", EditorStyles.boldLabel);
+        
+        GUILayout.BeginHorizontal();
+        upgradeName = EditorGUILayout.TextField("Name", upgradeName);
+        GUILayout.EndHorizontal();
+        
+        GUILayout.BeginHorizontal();
+        upgradeDescription = EditorGUILayout.TextField("Description", upgradeDescription);
+        GUILayout.EndHorizontal();
         
     }
 }
