71cf3004c135f43c937ec78d4dc3d6e47ee7b352
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-10-02 10:54:17 +0200

    Create AIMovement.cs

---
+++ Assets/Scripts/AI/AIMovement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovement : MonoBehaviour
+{
+    public Vector2 PathFind(Vector2 position, Vector2 targetPosition)
+    {
+        }
+    }
+
+}
