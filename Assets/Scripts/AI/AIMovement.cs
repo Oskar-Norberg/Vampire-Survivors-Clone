61a4bd59b9a717aa25423a1641520bc1384fe23e
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-10-02 10:54:39 +0200

    AIMovement enum

--- Assets/Scripts/AI/AIMovement.cs
+++ Assets/Scripts/AI/AIMovement.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class AIMovement : MonoBehaviour
 {
+    [SerializeField] private float oscillationSpeed = 1.0f;
+    [SerializeField] private float oscillationAmplitude = 1.0f;
+    
+    public enum AIType{ MoveTowards, Sinusodial }
+    private AIType aiType;
+
     public Vector2 PathFind(Vector2 position, Vector2 targetPosition)
     {
         }
