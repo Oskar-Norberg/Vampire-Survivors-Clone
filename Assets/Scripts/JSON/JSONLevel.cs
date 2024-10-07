e8062d5fb8dc5943a5c5db9c3859ef21c93bff34
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-10-07 12:06:21 +0200

    Create JSONLevel.cs

---
+++ Assets/Scripts/JSON/JSONLevel.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using File = System.IO.File;
+
+public static class JSONLevel
+{
+}
