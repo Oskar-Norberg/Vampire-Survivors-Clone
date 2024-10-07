dd6eccd39ee32565133a64e66cb80940d8273202
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-10-07 12:06:30 +0200

    Create LevelStats struct

--- Assets/Scripts/JSON/JSONLevel.cs
+++ Assets/Scripts/JSON/JSONLevel.cs
@@ -7,4 +7,16 @@ using File = System.IO.File;
 
 public static class JSONLevel
 {
+    [Serializable]
+    private struct LevelStats
+    {
+        public LevelStats(string name, float time)
+        {
+            this.name = name;
+            this.time = time;
+        }
+        
+        public string name;
+        public float time;
+    }
 }
