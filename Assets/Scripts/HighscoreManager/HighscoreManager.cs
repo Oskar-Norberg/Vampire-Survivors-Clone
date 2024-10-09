3930d8473b77c8a204e1d967b319548f0c1691cb
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-10-09 10:18:49 +0200

    Make HighScoreManager Singleton

--- Assets/Scripts/HighscoreManager/HighscoreManager.cs
+++ Assets/Scripts/HighscoreManager/HighscoreManager.cs
@@ -6,4 +6,16 @@ using UnityEngine;
 
 public class HighscoreManager : MonoBehaviour
 {
+    public static HighscoreManager instance;
+    private void Awake()
+    {
+        if (instance != null && instance != this) 
+        { 
+            Destroy(this); 
+        }
+        else 
+        { 
+            instance = this; 
+        } 
+    }
 }
