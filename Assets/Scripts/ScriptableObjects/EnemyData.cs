57f91a011741b788bda773bed96f374ba5751f36
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-10-09 16:05:37 +0200

    Add XPOrbData to EnemyData

--- Assets/Scripts/ScriptableObjects/EnemyData.cs
+++ Assets/Scripts/ScriptableObjects/EnemyData.cs
@@ -14,5 +14,5 @@ public class EnemyData : ScriptableObject
     public AIMovement.AIType aiType;
     public float moveSpeed;
 
-    public GameObject xpOrbPrefab;
+    public XPOrbData xpOrbData;
 }
