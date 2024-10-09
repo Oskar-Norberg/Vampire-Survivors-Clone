1ca0c108d8d07c3c52e12911f6abab9fc37718af
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-10-09 11:55:51 +0200

    Make HighScoreManager a static class

--- Assets/Scripts/HighscoreManager/HighscoreManager.cs
+++ Assets/Scripts/HighscoreManager/HighscoreManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class HighscoreManager : MonoBehaviour
+public static class HighscoreManager
 {
     public static HighscoreManager instance;
     
