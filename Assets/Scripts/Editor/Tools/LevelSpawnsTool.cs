584cf13868211a3208c40baf2940800caaeaf4e5
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-10-10 19:12:46 +0200

    Create LevelSpawnsTool.cs

---
+++ Assets/Scripts/Editor/Tools/LevelSpawnsTool.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSpawnsTool : CreationToolBase
+{
+}
