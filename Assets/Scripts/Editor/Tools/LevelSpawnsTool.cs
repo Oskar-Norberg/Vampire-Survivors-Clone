ef648ccd42fc6afdbd96f8b44080034281d5991d
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-10-10 19:13:17 +0200

    Load Scene

--- Assets/Scripts/Editor/Tools/LevelSpawnsTool.cs
+++ Assets/Scripts/Editor/Tools/LevelSpawnsTool.cs
@@ -10,9 +10,34 @@ using UnityEngine.SceneManagement;
 
 public class LevelSpawnsTool : CreationToolBase
 {
+    private const string LEVEL_SCENE_PATH = "Assets/Scenes/Levels";
+    
+    private List<string> levelPaths = new List<string>();
+    private int levelSelectionIndex = 0;
+    
+    private Scene currentScene;
+    private int currentLevelIndex = 0;
     [MenuItem("Tools/Set Level Spawns")]
     public static void ShowWindow()
     {
         EditorWindow.GetWindow<LevelSpawnsTool>("Level Spawns Tool");
+    }
+    
+    private void OnGUI()
+    {
+        LoadScene();
+    }
+    private void LoadScene()
+    {
+        levelPaths = FindAllAssetsInPath("scene", LEVEL_SCENE_PATH);
+        
+        BoldLabel("Select Level Spawns");
+
+        DropdownFromStringList("Select Level", ref levelSelectionIndex, levelPaths);
+
+        if (!currentScene.isLoaded || levelSelectionIndex != currentLevelIndex)
+        {
+            currentScene = EditorSceneManager.OpenScene(levelPaths[levelSelectionIndex]);
+        }
     }
 }
