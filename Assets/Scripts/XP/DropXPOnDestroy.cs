5c31435a10a1cc360fc563e3549a144d85682519
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-10-10 12:00:13 +0200

    Instantiate xp on destroy

--- Assets/Scripts/XP/DropXPOnDestroy.cs
+++ Assets/Scripts/XP/DropXPOnDestroy.cs
@@ -8,6 +8,21 @@ public class DropXPOnDestroy : MonoBehaviour
     private XPOrbData xpOrbData;
     [SerializeField] private GameObject xpPrefab;
 
+    private void OnDestroy()
+    {
+        if(!this.gameObject.scene.isLoaded) return;
+        GameObject xpInstance = Instantiate(xpPrefab, transform.position, transform.rotation);
+        if (xpInstance.TryGetComponent<XPOrb>(out XPOrb xpOrb))
+        {
+            xpOrb.SetXPOrbData(xpOrbData);
+        }
+        else
+        {
+            Debug.Log("Could not get XPOrb Component");
+        }
+    }
+
+    public void SetXPOrbData(XPOrbData newXPOrbData)
     {
         xpOrbData = newXPOrbData;
     }
