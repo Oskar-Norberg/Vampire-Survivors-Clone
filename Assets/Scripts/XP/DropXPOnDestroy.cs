9d8f8dc0f4de156cec4a64f1627ba7ba728bf5de
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-10-10 12:23:04 +0200

    Put all XPOrbs under a single parent in scene view

--- Assets/Scripts/XP/DropXPOnDestroy.cs
+++ Assets/Scripts/XP/DropXPOnDestroy.cs
@@ -7,15 +7,21 @@ public class DropXPOnDestroy : MonoBehaviour
 {
     private XPOrbData xpOrbData;
     [SerializeField] private GameObject xpPrefab;
-
+    
+    private static Transform _parent;
+    private const string ParentName = "XP Orbs";
+    
     private void OnDestroy()
     {
         if(!this.gameObject.scene.isLoaded) return;
         
         if(!xpOrbData) Debug.LogWarning("DropXPOnDestroy: xpOrbData is null");
         if(!xpPrefab) Debug.LogWarning("DropXPOnDestroy: xpPrefab is null");
+        if (!_parent) CreateXPParent();
         
-        GameObject xpInstance = Instantiate(xpPrefab, transform.position, transform.rotation);
+        GameObject xpInstance = Instantiate(xpPrefab, _parent);
+        xpInstance.transform.SetParent(_parent);
+        xpInstance.transform.position = transform.position;
 
         if (xpInstance.TryGetComponent<XPOrb>(out XPOrb xpOrb)) xpOrb.SetXPOrbData(xpOrbData);
         else Debug.Log("Could not get XPOrb Component");
@@ -24,5 +30,12 @@ public class DropXPOnDestroy : MonoBehaviour
     public void SetXPOrbData(XPOrbData newXPOrbData)
     {
         xpOrbData = newXPOrbData;
+    }
+    
+    // Puts all XP Orbs in hierarchy under a single parent
+    private void CreateXPParent()
+    {
+        _parent = new GameObject().transform;
+        _parent.name = ParentName;
     }
 }
