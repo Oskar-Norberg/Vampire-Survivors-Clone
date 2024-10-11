8c9c9afbb04756c5e21c63a30f7233260095f112
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-10-11 11:03:05 +0200

    PlaySoundOnDestroy script

---
+++ Assets/Scripts/Audio/PlayRandomSoundOnDestroy.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlaySoundOnDestroy : MonoBehaviour
+{
+    private List<AudioClip> audioClips = new List<AudioClip>();
+
+    private void OnDestroy()
+    {
+        int index = Random.Range(0, audioClips.Count);
+        
+        AudioManager.instance.PlaySoundOneShot(audioClips[index], transform);
+    }
+}
