bf70540b7600085557fa57e0b8e3c87bc5292d00
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2024-10-11 12:07:04 +0200

    Add AudioType to PlayRandomSoundOnDestroy

--- Assets/Scripts/Audio/PlayRandomSoundOnDestroy.cs
+++ Assets/Scripts/Audio/PlayRandomSoundOnDestroy.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using Random = UnityEngine.Random;
 
 public class PlaySoundOnDestroy : MonoBehaviour
 {
+    [SerializeField] private AudioManager.AudioType type;
     [SerializeField] private List<AudioClip> audioClips = new List<AudioClip>();
 
     private void OnDestroy()
@@ -13,6 +15,6 @@ public class PlaySoundOnDestroy : MonoBehaviour
         if(!this.gameObject.scene.isLoaded) return;
         int index = Random.Range(0, audioClips.Count);
         
-        AudioManager.instance.PlaySoundOneShot(audioClips[index], transform);
+        AudioManager.instance.PlaySoundOneShot(audioClips[index], type, transform);
     }
 }
